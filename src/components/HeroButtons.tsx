8945bf34332b784bb2c8606a1c9356acffbafe33
kTz1 <catalin_simon90@yahoo.com>
2025-10-08 16:39:26 +0300

    refactor HeroButtons component to improve code readability and reuse scrollToSection function

--- src/components/HeroButtons.tsx
+++ src/components/HeroButtons.tsx
@@ -1,15 +1,16 @@
 import ContactIcon from "@/assets/icons/contact.svg";
 import ArrowDown from "@/assets/icons/arrow-down.svg";
 
+const scrollToSection = (sectionId: string) => {
+  document.getElementById(sectionId)?.scrollIntoView({ behavior: "smooth" });
+};
+
 const HeroButtons = ({ className = "" }: { className?: string }) => (
-  <div className={className}>
+  <div className={`flex gap-4 ${className}`}>
     <button
+      type="button"
       className="inline-flex items-center gap-2 border border-white/15 px-6 h-12 rounded-xl cursor-pointer"
-      onClick={() =>
-        document
-          .getElementById("projects")
-          ?.scrollIntoView({ behavior: "smooth" })
-      }
+      onClick={() => scrollToSection("projects")}
     >
       <span className="font-semibold">My Work</span>
       <ArrowDown className="size-4 animate-bounce" />
@@ -18,11 +19,7 @@ const HeroButtons = ({ className = "" }: { className?: string }) => (
     <button
       type="button"
       className="inline-flex items-center gap-2 border border-white bg-white text-gray-900 h-12 px-6 rounded-xl cursor-pointer"
-      onClick={() =>
-        document
-          .getElementById("contact")
-          ?.scrollIntoView({ behavior: "smooth" })
-      }
+      onClick={() => scrollToSection("contact")}
     >
       <ContactIcon className="size-6" />
       <span className="font-semibold">Contact</span>
